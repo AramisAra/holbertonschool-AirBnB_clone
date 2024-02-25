705fa084399e3fffeb3c7cace4ceb78f40d44a40
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-25 14:14:32 -0400

    Adding Doc

--- models/base_model.py
+++ models/base_model.py
@@ -9,11 +9,23 @@ class BaseModel():
     """
 
     def __init__(self):
+        """
+        Initializes a new instance of the BaseModel class.
+
+        Attributes:
+            id (str): The unique identifier of the instance.
+            created_at (datetime): The date and time when the instance was created.
+            updated_at (datetime): The date and time when the instance was last updated.
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
 
     def __str__(self):
+        """
+        Returns a string representation of the object.
+        The string includes the class name, the object's ID, and its attributes.
+        """
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
