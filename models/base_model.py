482e15cebc56166e1b96381be9f934dc75bec620
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-27 16:31:26 -0400

    Adding the base update

--- models/base_model.py
+++ models/base_model.py
@@ -9,18 +9,35 @@ class BaseModel():
     Class representing the base model for all other models in the project.
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes a new instance of the BaseModel class.
 
+        Args:
+            *args: Unu.
+            **kwargs: Arbitrary keyword arguments.
+
         Attributes:
             id (str): The unique identifier of the instance.
-            created_at (datetime): The date and time when the instance created.
-            updated_at (datetime): Date/time when the instance last updated.
+            created_at (datetime): The date and time when the instance was created.
+            updated_at (datetime): The date and time when the instance was last updated.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ('created_at', 'updated_at'):
+                        setattr(self, key, datetime.datetime.strptime(
+                            value, '%Y-%m-%dT%H:%M:%S.%f'))
+                    else:
+                        setattr(self, key, value)
+            if 'created_at' not in kwargs:
+                self.created_at = datetime.datetime.now()
+            if 'updated_at' not in kwargs:
+                self.updated_at = datetime.datetime.now()
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.datetime.now()
+            self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """
