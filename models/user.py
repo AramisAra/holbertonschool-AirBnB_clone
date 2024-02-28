a11c8c44b3d540f24a1be99b4f5c0e950b70491a
Aramis Martinez <martinezcruzaj@gmail.com>
2024-02-27 21:52:53 -0400

    adding the user class

---
+++ models/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" User class """
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ all of user attributes """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.email = ""
+        self.password = ""
+        self.first_name = ""
+        self.last_name = ""
